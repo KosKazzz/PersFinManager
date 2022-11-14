ecd0d4be4a9994a6e092bec8c53181d102caa886
KosKazzz <xuzzzz@gmail.com>
2022-11-14 22:22:18 +0300

    fix encoding error

--- src/test/java/ru/koskazzz/finmanager/PersonalFinManTest.java
+++ src/test/java/ru/koskazzz/finmanager/PersonalFinManTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class PersonalFinManTest {
 
     PersonalFinMan personalFinMan = new PersonalFinMan();
-    String jsonString = "{\"date\":\"2022.10.30\",\"sum\":\"1200\",\"title\":\"шапка\"}";
+    String jsonTestString = "{\"date\":\"2022.10.30\",\"sum\":\"1200\",\"title\":\"шапка\"}";
 
     @Test
     public void maxCategoryTest() {
@@ -16,18 +16,17 @@ public class PersonalFinManTest {
         expectedJsonObjectDetails.put("sum", 2000.0);
         JSONObject expectedJsonObject = new JSONObject();
         expectedJsonObject.put("maxCategory", expectedJsonObjectDetails);
-        personalFinMan.addPurchaseList(jsonString);
+        personalFinMan.addPurchaseList(jsonTestString);
         personalFinMan.addPurchaseList("{\"date\":\"2022.10.30\",\"sum\":\"1700\",\"title\":\"булка\"}");
         personalFinMan.addPurchaseList("{\"date\":\"2022.10.30\",\"sum\":\"200\",\"title\":\"булка\"}");
         personalFinMan.addPurchaseList("{\"date\":\"2022.10.30\",\"sum\":\"100\",\"title\":\"булка\"}");
         Assertions.assertEquals(expectedJsonObject, personalFinMan.maxCategory(personalFinMan.getPurchaseList()));
-
     }
 
     @Test
     public void fieldOfPurchaseTest() {
-        String actual = personalFinMan.fieldOfPurchase(jsonString, "sum");
-        String actual1 = personalFinMan.fieldOfPurchase(jsonString, "title");
+        String actual = personalFinMan.fieldOfPurchase(jsonTestString, "sum");
+        String actual1 = personalFinMan.fieldOfPurchase(jsonTestString, "title");
         Assertions.assertEquals("1200", actual);
         Assertions.assertEquals("шапка", actual1);
     }
